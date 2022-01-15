a9e10a1a56c836e3f49ee8c158b78984559b856f
Tiago Oliveira <tiagoliveira555@gmail.com>
2022-01-15 15:42:44 -0300

    MovieCard

---
+++ frontend/src/components/MovieStarts/index.tsx
@@ -0,0 +1,19 @@
+import { ReactComponent as StarFull } from 'assets/img/start-full.svg';
+import { ReactComponent as StarHalf } from 'assets/img/start-half.svg';
+import { ReactComponent as StarEmpty } from 'assets/img/start-empty.svg';
+import './styles.css';
+
+function MovieStarts() {
+
+    return (
+        <div className="dsmovie-stars-container">
+            <StarFull />
+            <StarFull />
+            <StarFull />
+            <StarHalf />
+            <StarEmpty />
+        </div>
+    );
+}
+
+export default MovieStarts;
